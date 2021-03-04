932720da234902fec92d2f0331179523958986d6
amoljagadambe <amol.jagdambe@gmail.com>
2021-03-04 12:37:31 +0530

    original state

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn and best React
+          Learn React
         </a>
       </header>
     </div>
